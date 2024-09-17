341b000b410f587a31a90ad0d2de4df93867c921
Morgan428 <MorganWare428@gmail.com>
2024-09-17 14:50:23 -0700

    Update ClockDisplaySeconds.java
    Done!

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,73 @@
+package src.main.java;
+
 
 public class ClockDisplaySeconds
 {
-    //Extend ClockDisplaySeconds to include a seconds field.
-    //Update all methods and parameters to accomodate this change
+    //Implement 2 private NumberDisplay objects. 
+    //  One for hours and one for minutes
+    //Implement a private String for the display
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private NumberDisplay seconds;
+    private String display;
+    //Implement a constructor that takes no parameters
+    //The constructor should set hours as a NumberDisplay object with 24 as the limit
+    //The constructor should set minutes as a NumberDisplay object with 60 as the limit
+    //The constructor should call the method updateDisplay before finishing
+    public ClockDisplaySeconds(){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        updateDisplaySeconds();
+    }
+    //Implement a constructor that takes 2 parameters, hour and minute
+    //The constructor should set hours as a NumberDisplay object with 24 as the limit
+    //The constructor should set minutes as a NumberDisplay object with 60 as the limit
+    //The constructor should call the method setTime with the parameters passed in
+    public ClockDisplaySeconds(int hour, int minute, int second){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        setTimeSeconds(hour, minute, second);
+    }
+    //Implement a method timeTick that takes no parameters and returns nothing
+    //The method should increase the minute value by one each run
+    //The hours should increase when the minutes roll over
+    //The updateDisplay method should be called before finishing
+    public void timeTickSeconds(){
+        seconds.incerment();
+        if (seconds.getValue() == 0){
+            minutes.incerment();
+        }
+        if (minutes.getValue() == 0){
+            hours.incerment();
+        }
+        updateDisplaySeconds();
+    }
+    //Implement a method setTime that takes in 2 parameters, hour and minute
+    //The method should set the hours value and minutes value
+    //The updateDisplay method should be called before finishing
+    public void setTimeSeconds(int hour, int minute, int second){
+        minutes.setValue(minute);
+        hours.setValue(hour);
+        seconds.setValue(second);
+        updateDisplaySeconds();
+    }    
+    //Implement a method getTime that takes no parameter and returns a String
+    //The return String should be formatted as HH:MM and report out the current time
+    public String getTimeSeconds(){
+     return display;
+    }
+    //Implement a method updateDisplay that takes no parameters and returns nothing
+    //The method should update the displayString with the current time in a format
+    //  HH:MM
+    public void updateDisplaySeconds(){
+        String output;
+        output = hours.getDisplayValue();
+        output = output + ":";
+        output = output + minutes.getValue();
+        output = output + ":";
+        output = output + seconds.getValue();
+        display = output;
+        }
 }
